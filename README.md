4a95f09d6c0036de1f021544a326b54ddafc1660
Andrew Marshall <andrew@johnandrewmarshall.com>
2013-02-12 23:40:08 -0500

    Add tests to document the varying behavior of private method calls

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ BlockChain.new(:foo, :bar, :baz, :qux).call(self) { yield }
 
 With the last form, the argument given to `call` is the receiver of the methods.
 
-This can make deeply-nested block wrappings a bit more digestable.
+This can make deeply-nested block wrappings a bit more digestable. For more details, read the specs, they’re short and descriptive.
 
 ## Contributing
 
--- spec/block_chain_spec.rb
+++ spec/block_chain_spec.rb
@@ -31,6 +31,35 @@ describe BlockChain do
     block_chain.call(self) { 'x' }.should == 'f(g(h(x)))'
   end
 
+  it "cannot call private methods when passing a symbol" do
+    klass = Class.new do
+      def f; "f(#{yield})"; end
+      def g; "g(#{yield})"; end
+      def h; "h(#{yield})"; end
+      private :f, :g, :h
+    end
+    block_chain = BlockChain.new :f, :g, :h
+
+    -> do
+      block_chain.call(klass.new) { 'x' }
+    end.should raise_error(NoMethodError, /private method/)
+  end
+
+  it "can call private methods when passing a method" do
+    klass = Class.new do
+      def f; "f(#{yield})"; end
+      def g; "g(#{yield})"; end
+      def h; "h(#{yield})"; end
+      private :f, :g, :h
+    end
+    instance = klass.new
+    block_chain = BlockChain.new instance.method(:f), instance.method(:g), instance.method(:h)
+
+    -> do
+      block_chain.call { 'x' }
+    end.should_not raise_error(NoMethodError, /private method/)
+  end
+
   it "just yields when no methods are given" do
     BlockChain.new.call { 'x' }.should == 'x'
   end
